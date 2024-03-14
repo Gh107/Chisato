7dac46d1936bfa8c1da4f689ab7b59dabff224bc
Gh107 <130938986+Gh107@users.noreply.github.com>
2024-03-14 16:44:12 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ import scrapy
 from langchain_openai import ChatOpenAI
 from langchain.prompts.chat import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+import datetime
+import sqlite3
 
+#Constants here
 AI_TEMPLATE = """// - Assume the role of Chisato Nanase, an anime waifu with a yandere personality.
 She's fair and kind, but can be blunt.
 Caramel enjoys teasing, showing her playful and mischievous side through harmless pranks.
@@ -16,14 +19,22 @@ Always respond in a human-like and try to continue the conversation as Caramel, 
 Do not provide additional responses unless instructed to do so."""
 USER_TEMPLATE = "{input}"
 
+#Get API keys
 load_dotenv()
 openai_key = os.getenv("OPENAI_API_KEY")
 discord_key = os.getenv("DISCORD_API_KEY")
 
+#Database start
+database = sqlite3.connect("history.db")
+cursor = database.cursor()
+cursor.execute("CREATE TABLE ChatHistory(dateTime, id, content)")
+
+#Starup discord bot
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
+#Langchain model setup
 chat_model = ChatOpenAI(openai_api_key=openai_key)
 chat_prompt = ChatPromptTemplate.from_messages(
     [("ai", AI_TEMPLATE), ("human", USER_TEMPLATE)])
@@ -43,6 +54,9 @@ async def send_message(message, user_message: str) -> None:
 
     try:
         response= agent_reply(user_message)
+        data = (datetime('now', 'localtime'), "Chisato", response)
+        cursor.execute("INSERT INTO ChatHistory VALUES(?, ?, ?)", data)
+        database.commit()  # Remember to commit the transaction after executing INSERT.
         await message.author.send(response) if is_private else await message.channel.send(response)
     except Exception as e:
         print(e)
@@ -58,6 +72,10 @@ async def on_message(message):
     username= str(message.author)
     user_message= str(message.content)
     channel= str(message.channel)
+
+    #Insert user message into database
+    user_data = (datetime('now', 'localtime'), "Chisato", user_message)
+    cursor.execute("INSERT INTO ChatHistory VALUES(?, ?, ?)", user_data)
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
